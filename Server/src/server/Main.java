20b6ce3eaff1e7e22741f5b47a3b7be4c7130415
alexandre <alexandre.faltot@isen-lille.fr>
2016-04-15 21:59:31 -0400

    Add the server

---
+++ Server/src/server/Main.java
@@ -0,0 +1,11 @@
+package server;
+
+/**
+ * Created by alexandre on 15/04/16.
+ */
+public class Main {
+    public static void main(String[] args){
+        Server server = new Server(2016);
+        server.startServer();
+    }
+}
--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -1,7 +1,45 @@
 package server;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
- * Created by Arnaud on 15/04/2016.
+ * Created by alexandre on 15/04/16.
  */
 public class Server {
+    private Socket clientSock;
+    private static ServerSocket serverSocket;
+
+    /**
+     * Create a server that listens on the port defined
+     * @param port
+     */
+    public Server(int port) {
+        try{
+            //Create the ServerSocket
+            serverSocket = new ServerSocket(port);
+            System.out.println("Listening to port " + port);
+        } catch(IOException e) {
+            //If the serveur can't open a socket, display an error message
+            System.out.println("[ERROR] Couldn't open a socket on port " + port + ".");
+            System.exit(-2);
+        }
+    }
+
+    /**
+     * Starts the server
+     */
+    public void startServer(){
+        //The server runs continuously
+        while(true) {
+            try {
+                //Accept the connection from the client
+                clientSock = serverSocket.accept();
+                new ClientHandler(clientSock).run();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
